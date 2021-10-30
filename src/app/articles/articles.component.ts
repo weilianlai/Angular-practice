0f21060802934c97cfdd774e3ed82bce4777b167
weilian <weilian@gmail.com>
2021-10-30 14:28:30 +0800

    任務 09 使用 @ 傳遞資料給子元件

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.css']
 })
+
 export class ArticlesComponent implements OnInit {
+  @Input() articles!: any[] ;
 
   constructor() { }
-
   ngOnInit(): void {
+
   }
 
 }
